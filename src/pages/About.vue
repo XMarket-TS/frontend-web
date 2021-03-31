f19e54c182b4d608a9f5079d80c1b6e1758860c7
Fulano2141 <macuto2142dt@gmail.com>
2021-03-31 19:22:47 -0400

    Changes

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -5,6 +5,7 @@
         <v-col cols="12">
           <v-card max-width="95%" class="mx-auto">
             <v-card-title>This is an about page</v-card-title>
+            <v-card-text>About text</v-card-text>
           </v-card>
         </v-col>
       </v-row>
